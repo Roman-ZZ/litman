af437355852ec51dd106c3b6679bf513982dcebc
Roman-ZZ <1943801648@qq.com>
2021-08-13 20:21:00 +0800

    add dropdown into user status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :email, :password, :name, presence: true, length: { maximum: 255 }
-  validates :status, inclusion: {in: [0,1]}
+  # validates :status, inclusion: {in: [0,1]}
+  enum status: [:active, :inactive ], _suffix: true
 end
