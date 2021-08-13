889bd64678f1ee046376291244e4c3c962a41aaa
Roman-ZZ <1943801648@qq.com>
2021-08-13 21:04:41 +0800

    add relation user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :email, :password, :name, presence: true, length: { maximum: 255 }
   # validates :status, inclusion: {in: [0,1]}
-  enum status: [:active, :inactive ], _suffix: true
+  enum status: [:active, :inactive ]
+
+  has_many: :articles
 end
