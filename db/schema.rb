880c3ed6cebf2775e9f40649fbe40998b30658c7
Roman-ZZ <1943801648@qq.com>
2021-08-13 20:59:07 +0800

    use scaffold create article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_124023) do
+ActiveRecord::Schema.define(version: 2021_08_13_125656) do
+
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email"
