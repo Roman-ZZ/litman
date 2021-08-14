2800e2d08d5f3ed9d8304baefd64ee95b6d6c311
Roman-ZZ <1943801648@qq.com>
2021-08-14 17:11:47 +0800

    update animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,21 @@
 class Animal < ApplicationRecord
+
+    # response = HTTParty.get('https://axoltlapi.herokuapp.com/ ')
+
+# puts response.body, response.code, response.message, response.headers.inspect
+
+  include HTTParty
+  
+  def self.fetch
+    response = HTTParty.get('https://axoltlapi.herokuapp.com/')
+    response = JSON.parse(response.to_json)
+    animal = Animal.new(
+      url:response["url"].to_s,
+      facts:response["facts"].to_s,
+      pics_repo:response["pics_repo"].to_s,
+      api_repo:response["api_repo"].to_s
+      )
+    animal.save!
+    # Animal.create([])
+  end
 end
